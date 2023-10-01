6fc00b97a4885e678f04168dd0fa32976fd90253
ochobkaa <creepersyndicate@yandex.ru>
2023-10-01 13:23:38 +0300

    Добавил проверку, что тип экшона не абстрактный, инстанс создается только в случае если найден атрибут BotActionAttribute

--- ActionExtensions.cs
+++ ActionExtensions.cs
@@ -14,16 +14,19 @@ namespace GenshinVybyu
             var actionsCollection = builder.ServiceProvider.GetService<IActionsCollection>();
 
             var actions = Assembly.GetExecutingAssembly().GetTypes()
-                .Where(t => t.GetInterface("IBotAction") == typeof(IBotAction));
+                .Where(t => t.GetInterface("IBotAction") != null)
+                .Where(t => !t.IsAbstract);
             if (actions == null) return;
 
             foreach (var action in actions)
             {
-                var actionObj = Activator.CreateInstance(action) as IBotAction;
                 ActionCommand command = action.GetCustomAttribute<BotActionAttribute>().Command;
 
                 if (command != null)
+                {
+                    var actionObj = Activator.CreateInstance(action) as IBotAction;
                     actionsCollection.Bind(command, actionObj);
+                }
             }
         }
     }
